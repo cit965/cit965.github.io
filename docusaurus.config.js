cbf67c29832505af7d804fb9a36ccdfbecc5fe8e
mouuii <chaoyueshijian@gmail.com>
2024-11-27 13:50:33 +0800

    add github action

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,8 +22,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'cit965', // Usually your GitHub org/user name.
+  projectName: 'cit965.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
